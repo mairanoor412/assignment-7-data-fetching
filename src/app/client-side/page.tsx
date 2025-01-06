b37c108cfa119b562fa4c932c1c178de2a08136b
Maira Naeem <mairanoor412@gmail.com>
2025-01-06 20:42:41 +0500

    add a console

--- src/app/client-side/page.tsx
+++ src/app/client-side/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 
 
@@ -40,17 +41,18 @@ const ClientSide = () => {
 
     return (
         <div>
-            <div className="mt-28 grid grid-cols-1 justify-items-center gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 rounded-[5px] bg-white ">
+            <div className="mt-12 grid grid-cols-1 justify-items-center gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 rounded-[5px] bg-white ">
                 {loading ? (
                     <div className="h-[100vh] flex justify-center items-center col-span-4">
                         <div className="w-8 h-8 border-4 border-t-4 border-gray-300 border-t-yellow-500 rounded-full animate-spin"></div>
                     </div>
                 ):( data.map((products, index) => (
-                    <div key={index}>
+                    <div key={index} className="transition-transform transform hover:scale-105 hover:translate-y-2 duration-500 ease-out">
                         {/* container */}
 
                         {/* card */}
                         <div className="w-[285px] sm:w-[320px] h-auto bg-white shadow-xl rounded-[10px] px-3 py-5">
+                        <Link href={`/products/${products.id}`} target="_blank">
                             <Image src={products.image} alt={products.title} width={250} height={200} className="w-[250px] h-[200px] m-auto py-2" />
                             <h1 className="text-[20px]  font-[600] mt-4 line-clamp-1 text-black"> {products.title} </h1>
                             <div className="h-[20px] mt-2 flex justify-between items-center">
@@ -58,7 +60,13 @@ const ClientSide = () => {
                                 <p className="text-[12px] text-yellow-500 ">{products.rating.rate} ({products.rating.count} reviews)</p>
                             </div>
                             <p className="my-3 text-[16px] line-clamp-2 text-black"> {products.description} </p>
-                            <p className="text-[20px] font-[500] text-green-700"> ${products.price} </p>
+                            <div className="flex items-center justify-between">
+                                    <p className="text-[20px] font-[500] text-green-700"> ${products.price} </p>
+
+                                    <p className="hover:font-[600] pr-5"> Detail </p>
+
+                                </div>
+                            </Link>
                             <button className="w-[250px] sm:w-[280px] py-[8px] text-[12px] lg:text-[14px] bg-yellow-500 hover:bg-yellow-600 rounded-[10px] text-white mt-3"> Add To Card </button>
 
                         </div>
---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+
+
+
+interface Products {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: {
+        rate: number;
+        count: number;
+    }
+}
+
+
+
+const Products =async ({params}:any)=>{
+const response = await fetch(`https://fakestoreapi.com/products/${params.slug}`)
+const ParsedResponse:Products = await response.json()
+console.log(ParsedResponse);
+
+    return(
+        <div className="mb-10 flex flex-col px-3 sm:px-7  lg:px-52 xl:px-72 gap-3 lg:gap-5">
+           <Image src={ParsedResponse.image} alt={ParsedResponse.title} width={250} height={250} className="w-[250px] h-[250px] m-auto mt-12 mb-6" />
+           <p className="text-[30px] font-[600]">{ParsedResponse.title} </p>
+          <p className="text-[16px]">{ParsedResponse.description}</p>
+          <p className="text-[25px] font-[600] text-green-600"> ${ParsedResponse.price}</p>
+          <button className="w-[250px] sm:w-[280px] py-[8px] text-[12px] lg:text-[14px] bg-yellow-500 hover:bg-yellow-600 rounded-[10px] text-white mt-3"> Add To Card </button>
+         
+        </div>
+    )
+}
+
+export default Products;
+
+
