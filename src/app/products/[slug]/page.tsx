3c3a1b716e34784040d322f01434acc08baf0b59
Maira Naeem <mairanoor412@gmail.com>
2025-01-06 20:50:34 +0500

    add a console

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -23,10 +23,10 @@ const ParsedResponse:Products = await response.json()
 console.log(ParsedResponse);
 
     return(
-        <div className="bg-white mb-10 flex flex-col px-3 sm:px-7  lg:px-52 xl:px-72 gap-3 lg:gap-5">
+        <div className="bg-white pb-10 flex flex-col px-3 sm:px-7  lg:px-52 xl:px-72 gap-3 lg:gap-5">
            <Image src={ParsedResponse.image} alt={ParsedResponse.title} width={250} height={250} className="w-[250px] h-[250px] m-auto mt-12 mb-6" />
-           <p className="text-[30px] font-[600]">{ParsedResponse.title} </p>
-          <p className="text-[16px]">{ParsedResponse.description}</p>
+           <p className="text-[30px] font-[600] text-black">{ParsedResponse.title} </p>
+          <p className="text-[16px] text-black">{ParsedResponse.description}</p>
           <p className="text-[25px] font-[600] text-green-600"> ${ParsedResponse.price}</p>
           <button className="w-[250px] sm:w-[280px] py-[8px] text-[12px] lg:text-[14px] bg-yellow-500 hover:bg-yellow-600 rounded-[10px] text-white mt-3"> Add To Card </button>
          
